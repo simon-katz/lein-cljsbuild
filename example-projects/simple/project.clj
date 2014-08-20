edcaa4ef61d8d06be5c0a25367c39467e9ca504d
Simon Katz <sk@nomistech.com>
2014-08-20 15:20:19 +0100

    Add dependency on Ring so I can load the project into a REPL. (It was depending on the lein-ring plugin pulling in Ring.)

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2197"
                   :exclusions [org.apache.ant/ant]]
+                 [ring/ring "1.2.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]]
   :plugins [[lein-cljsbuild "1.0.3"]
