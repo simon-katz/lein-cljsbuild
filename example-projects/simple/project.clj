ddf42a5f2327b091f73c2477c174348b01034afe
Simon Katz <sk@nomistech.com>
2014-08-20 15:56:21 +0100

    It didn't work. Had to change dependencies to earlier versions for clojurescript and lein-cljsbuild. Someone else had similar problems: https://www.marshut.net/khqypz/namespace-errors-when-upgrading-to-2197.html

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -2,12 +2,13 @@
   :description "A simple example of how to use lein-cljsbuild"
   :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2197"
-                  :exclusions [org.apache.ant/ant]]
+                 [org.clojure/clojurescript "0.0-2173"
+                  ;; :exclusions [org.apache.ant/ant]
+                  ]
                  [ring/ring "1.2.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]]
-  :plugins [[lein-cljsbuild "1.0.3"]
+  :plugins [[lein-cljsbuild "1.0.2"]
             [lein-ring "0.8.7"]]
   :cljsbuild {
     :builds [{:source-paths ["src-cljs"]
